08c564835ffd980bff5151fb89fc3a4ca46c73b6
glendont <glendon888@gmail.com>
2020-10-13 18:14:46 +0800

    Added bootstrap link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Fragment } from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavigationBar from "./components/layout/NavigationBar";
 import Home from "./components/pages/Home";
--- src/components/layout/Infographic.js
+++ src/components/layout/Infographic.js
@@ -1,6 +1,6 @@
 import React, {Fragment} from "react";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
